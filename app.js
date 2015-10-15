4d023ea6258ab6900d1545de8fed2d66ff0e08de
vickkiey <vickkiey0@hotmail.com>
2015-10-14 23:30:15 -0400

    commit 5

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -28,8 +29,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 
-
-
+app.use('/contact', routes);
+app.use('/services', routes);
+app.use('/about', routes);
+app.use('/projects', routes);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
